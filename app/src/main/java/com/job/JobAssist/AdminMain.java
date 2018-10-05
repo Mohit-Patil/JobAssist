f00fd90a89d0627418d8a35f3cdb37f5f9957421
mohit-patil <mohit_patil@outlook.in>
2018-10-05 21:25:39 +0530

    Fixes
    Signed-off-by: mohit-patil <mohit_patil@outlook.in>

--- app/src/main/java/com/job/JobAssist/AdminMain.java
+++ app/src/main/java/com/job/JobAssist/AdminMain.java
@@ -9,6 +9,6 @@ public class AdminMain extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-      //  setContentView(R.layout.activity_aboutus);
+        setContentView(R.layout.activity_adminmain);
     }
 }
